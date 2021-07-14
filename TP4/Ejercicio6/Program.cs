4ed23093ce82ef06c6ca761fdef638c2d01430b3
Jorge <jsosa2007>
2021-07-14 12:32:09 -0300

    Documento los métodos + paso la inicialización a un método

--- TP4/Ejercicio6/Program.cs
+++ TP4/Ejercicio6/Program.cs
@@ -5,20 +5,62 @@ namespace Ejercicio6
 {
     class Program
     {
-        private const string ShipWithBomb    = "[I]";
-        private const string ShipWithoutBomb = "[ ]";
+        // Constants
+        private const string ShipWithBomb    = "|--[I]--|";
+        private const string ShipWithoutBomb = "|--[ ]--|";
         private const string Bomb            =  "I";
         
+        // Methods
+        
+        /// <summary>
+        /// Inicializa la consola haciendo que el cursor no esté visible, limpiando la pantalla, seteando la
+        /// posición del cursor en cero respecto al ancho y en el centro respecto del alto de la consola
+        /// y muestra la nave con la bomba en su interior 
+        /// </summary>
+        private static void Initialize()
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.SetCursorPosition(0, GetWindowHeightMiddle());
+            Draw(ShipWithBomb);
+        }
+        
+        /// <summary>
+        /// Chequea que la variable presionada no sea la correspondiente al Escape
+        /// </summary>
+        /// <param name="keyPressed">Parámetro que corresponde a la tecla presionada</param>
+        /// <returns>
+        ///     <list type="true|false">
+        ///         <item>
+        ///             <term>true</term>
+        ///             <description>Si la tecla presionada no corresponde al Escape</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>false</term>
+        ///             <description>Si la tecla presionada corresponde al Escape</description>
+        ///         </item>
+        ///     </list>
+        /// </returns>
         static bool KeyPressedIsNotEscape(ConsoleKey keyPressed)
         {
             return ConsoleKey.Escape != keyPressed;
         }
         
+        /// <summary>
+        /// Escribe en pantalla el texto indicado en el parámetro objectToDraw
+        /// </summary>
+        /// <param name="objectToDraw"></param>
         static void Draw(string objectToDraw)
         {
             Console.Write(objectToDraw);
         }
 
+        /// <summary>
+        /// Mueve la nave de acuerdo a la tecla presionada teniendo en cuenta el alto y ancho de la consola sin permitir
+        /// que se superen los extremos
+        /// </summary>
+        /// <param name="ship">Es la nave a dibujar</param>
+        /// <param name="keyPressed">Es la tecla presionada</param>
         static void MoveShip(string ship, ConsoleKey keyPressed)
         {
             Tuple<int, int> currentPosition = GetCurrentPosition();
@@ -72,6 +114,12 @@ namespace Ejercicio6
             //Draw(ship);
         }
 
+        /// <summary>
+        /// Lanza la bomba si la tecla que se presionó es la B 
+        /// </summary>
+        /// <param name="keyPressed">
+        /// keyPressed: Es la tecla presionada
+        /// </param>
         public static void DropBomb(ConsoleKey keyPressed)
         {
             if (ConsoleKey.B == keyPressed)
@@ -85,7 +133,7 @@ namespace Ejercicio6
                 {
                     Console.SetCursorPosition(left - ShipWithoutBomb.Length, shipPositionTop);
                     Draw(ShipWithoutBomb);
-                    Console.SetCursorPosition(left - ShipWithoutBomb.Length + 1, bombTop + 1);
+                    Console.SetCursorPosition(left - (ShipWithoutBomb.Length + 1) / 2, bombTop + 1);
                     Draw(Bomb);
                     
                     bombTop = GetCurrentPosition().Item2;
@@ -100,40 +148,61 @@ namespace Ejercicio6
             }
         }
 
+        /// <summary>
+        /// Obtiene la posición actual del cursor en la consola
+        /// </summary>
+        /// <returns>
+        ///     Una tupla que corresponde al valor en X (Item1) y al valor en Y (Item2) de la posición actual del
+        ///     cursor
+        /// </returns>
         private static Tuple<int, int> GetCurrentPosition()
         {
             return Console.GetCursorPosition().ToTuple();
         }
         
+        /// <summary>
+        /// Calcula el centro de la consola respecto al largo
+        /// </summary>
+        /// <returns>
+        /// Valor entero correspondiente al centro de la consola
+        /// </returns>
         static int GetWindowHeightMiddle()
         {
             return Console.WindowHeight / 2;
         }
-        
+        /// <summary>
+        /// Calcula el centro de la consola respecto al ancho
+        /// </summary>
+        /// <returns>
+        /// Valor entero correspondiente al centro de la consola
+        /// </returns>
         static int GetWindowWidthMiddle()
         {
             return Console.WindowWidth / 2;
         }
 
-        private static void ShowExitMessage()
+        /// <summary>
+        /// Limpia la consola, muestra un mensaje y espera a que se presione una tecla 
+        /// </summary>
+        private static void ShowMessage(String message)
         {
-            string exitMessage = "Presione una tecla para salir...";
-            
             Console.Clear();
-            
-            Console.SetCursorPosition(GetWindowWidthMiddle() - exitMessage.Length/2, GetWindowHeightMiddle());
-            Console.Write(exitMessage);
+            Console.SetCursorPosition(GetWindowWidthMiddle() - message.Length/2, GetWindowHeightMiddle());
+            Console.Write(message);
             Console.ReadKey(true);
         }
         
+        // Main
+        /// <summary>
+        /// Hilo principal de ejecución
+        /// </summary>
+        /// <param name="args">Not in use</param>
         static void Main(string[] args)
         {
             ConsoleKey keyPressed = ConsoleKey.A;
-            Console.CursorVisible = false;
-            Console.Clear();
-            Console.SetCursorPosition(0, GetWindowHeightMiddle());
-            Draw(ShipWithBomb);
             
+            Initialize();
+
             do
             {
                 if (Console.KeyAvailable && KeyPressedIsNotEscape(keyPressed = Console.ReadKey().Key))
@@ -144,7 +213,7 @@ namespace Ejercicio6
 
             } while (KeyPressedIsNotEscape(keyPressed));
 
-            ShowExitMessage();
+            ShowMessage("Presione una tecla para salir...");
         }
     }
 }
